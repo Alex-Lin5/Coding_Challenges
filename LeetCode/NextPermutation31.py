0b1c80f11719c1fa8f3d16c3bdbedbfd08198e4e
Hero <jizlin575@gmail.com>
2023-12-28 18:11:01 +0800

    completed next permutation problem

--- LeetCode/NextPermutation31.py
+++ LeetCode/NextPermutation31.py
@@ -5,8 +5,10 @@ class Solution:
     def nextPermutation(self, nums: List[int]) -> List[int]:
         bpv = 0
         bpp = -1
-        for idx in range(len(nums)-1, 1, -1):
+        print("input: ", nums)
+        for idx in range(len(nums)-1, 0, -1):
             # already in ascending order on the bottom part
+            # print(idx)
             if(nums[idx]<=nums[idx-1]): continue
             bpp = idx-1
             bpv = nums[idx-1]
@@ -16,6 +18,7 @@ class Solution:
             return nums
         nov = 2**63
         nop = -1
+        print("located break point: ", bpp, bpv)
         for idx in range(len(nums)-1, bpp, -1):    
             # mark smallest number and index after break point
             if(nums[idx]<nov and nums[idx]>bpv):
@@ -29,6 +32,7 @@ class Solution:
             nums[bpp+1:] = tail
             # nums[bpp+1:].reverse() # wrong code
         print(bpv, bpp, nov, nop)
+        print("output: ", nums)
         return nums
 
 def test_answer():
@@ -39,7 +43,12 @@ def test_answer():
     assert S.nextPermutation([4,3,2,5,1,3]) == [4,3,2,5,3,1]
     assert S.nextPermutation([4,3,2,5,3,1]) == [4,3,3,1,2,5]
 
-test_answer()
+def debug():
+    S = Solution()
+    S.nextPermutation([1,3,2])
+
+debug()
+# test_answer()
 
 # For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
 # 4325 13 -> 43 2531 -> 43 3125
--- README.md
+++ README.md
@@ -1,11 +1,31 @@
 # Coding Chalanges
 A collection of coding problem solutions from different coding platforms or coding bank.
 
+### SSH connection
+```
+# generate SSH key with your email address
+ssh-keygen -t ed25519 -C "your_email@example.com"
+```
+```
+# adding SSH key in local machine
+ssh-add /c/Users/YOU/.ssh/id_ed25519
+```
+```
+# start the ssh-agent in the background
+$ eval "$(ssh-agent -s)"
+> Agent pid 59566
+```
+```
+# Find and take a note of your public key fingerprint.
+$ ssh-add -l -E sha256
+> 2048 SHA256:274ffWxgaxq/tSINAykStUL7XWyRNcRTlcST1Ei7gBQ /Users/USERNAME/.ssh/id_rsa (RSA)
+```
+
 ## Docker
 - `docker ps`, list all running containers
 - `docker-compose up --build`, rebuild and start up the docker services
-- `docker exec -it <container ID> sh`, interact the container of specific container ID with shell
-- `docker run -it <image ID> sh`, start a new container by image ID with shell
+- `docker exec -it <containerID> sh`, interact the container of specific container ID with shell
+- `docker run -it <imageID> sh`, start a new container in foreground by image ID with shell
 
 ## Testing
 - `$ pip install -U pytest`, install pytest testing framework
