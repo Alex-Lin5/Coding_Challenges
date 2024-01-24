1d408bb0dc32d9f88fc836f6d7e2d5019f9c2fa0
Hero <jizlin575@gmail.com>
2024-01-24 20:14:01 +0800

    added Manacher's algorithm on problem 5

--- LeetCode/LongestPalindromicSubstring5.py
+++ LeetCode/LongestPalindromicSubstring5.py
@@ -1,61 +1,41 @@
+import time
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # O(N^2)
-        # odd pattern
-        odd = []
-        for i in range(len(s)):
-            stride = 1
-            while True:
-                st = i-stride
-                ed = i+stride
-                if not (st>=0 and ed<len(s)): break
-                if(s[st] == s[ed]): 
-                    if stride == 1:
-                        odd.append([i, 3])
-                    odd[-1] = [i, 2*stride+1]
-                else: break
-                stride += 1
-        # even pattern
-        even = []
-        for i in range(len(s)):
-            stride = 1
-            while True:
-                st = i-stride+1
-                ed = i+stride
-                if not (st>=0 and ed<len(s)): break
-                if(s[st] == s[ed]): 
-                    if stride == 1:
-                        even.append([i, 2])
-                    even[-1] = [i, 2*stride]
-                else: break
-                stride += 1
-        
-        # find the longest palindrome substring
-        maxno = 0
-        cet = 0
-        pat = -1
-        for pr in odd:
-            if(pr[1]>maxno):
-                maxno = pr[1]
-                cet = pr[0]
-                pat = 1
-        for pr in even:
-            if(pr[1]>maxno):
-                maxno = pr[1]
-                cet = pr[0]
-                pat = 0
-        # generate substring answer
-        # print(maxno, cet, pat, ", maxno, ceter, pattern")
-        substr = ""
-        if pat == 0:
-            wing = int(maxno/2)
-            for i in range(cet-wing+1, cet+wing+1):
-                substr += s[i]
-        else:
-            wing = int((maxno-1)/2)
-            for i in range(cet-wing, cet+wing+1):
-                substr += s[i]
-        return substr
+        # O(N), Manacher's Algorithm
+        # startt = time.time()
+        spad = '#'+'#'.join(s)+'#' # might take a lot time overhead padding string
+        rads = [0 for _ in range(len(spad))]
+        rd = ct = maxr = maxc = 0
+        # print(time.time() - startt)
+        # startt = time.time()
+        for i in range(1, len(spad)-1):
+            # check palindrom within palindrom to save time
+            # mirror = i-(i-ct)*2 = (ct-i)*2+i
+            mirror = 2*ct-i
+            if rd <= 1: pass
+            elif i+rads[mirror] < ct+rd:
+                rads[i] = rads[mirror]
+                continue
+            elif i+rads[mirror] >= ct+rd:
+                rads[i] = ct+rd-i
+            # print("step 1,", i, rads[i], ct, rd)
+            # find out the max radius for this character
+            while (i-rads[i]-1>=0 and i+rads[i]+1<len(spad) 
+                and spad[i-rads[i]-1] == spad[i+rads[i]+1]):
+                rads[i] += 1
+            # move the radius and center if current one stays out of boundary
+            if i >= ct+rd:
+                ct = i
+                rd = rads[i]
+            # mark the largest radius and its center
+            if rads[i] > maxr: 
+                maxr = rads[i]
+                maxc = i
+        # print("radiuses,", rads, spad[maxc])
+        # print(s, spad, '\n', maxc, maxr, ", center, radius")
+        # print(time.time() - startt)
+        if maxr<=1: return s[0]
+        return s[(maxc-maxr)//2:(maxc+maxr)//2]
 
 def test_answer():
     S = Solution()
@@ -65,12 +45,16 @@ def test_answer():
     assert S.longestPalindrome("cbbd") == "bb"
     assert S.longestPalindrome("bb") == "bb"
     assert S.longestPalindrome("aaaa") == "aaaa"
+    assert S.longestPalindrome("a") == "a"
+    assert S.longestPalindrome("ab") == "a"
+    assert S.longestPalindrome("def longestPalindrome(self, s: str) -> s") == "d"
 
 def debug():
     S = Solution()
     # S.longestPalindrome("babad")
     # S.longestPalindrome("bb")
-    S.longestPalindrome("aaaa") == "aaaa"
+    # S.longestPalindrome("aaaa")
+    S.longestPalindrome("def longestPalindrome(self, s: str) -> sngestPalindrome(self, s: str) -> sngestPalindrome(self, s: str) -> sngestPalindrome(self, s: str) -> sngestPalindrome(self, s: str) -> sngestPalindrome(self, s: str) -> sngestPalindrome(self, s: str) -> sngestPalindrome(self, s: str) -> sngestPalindrome(self, s: str) -> s")
 debug()
         
         
