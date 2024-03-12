7bd880343f1892c07c7761fce99ea1179203f012
Hero <jizlin575@gmail.com>
2024-03-12 20:38:25 +0800

    added Leetcode Problem 394 solution, single stack application

--- LeetCode/DecodeString394.py
+++ LeetCode/DecodeString394.py
@@ -1,54 +1,23 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        # Time O(k*N), where k is the number of embbeded layer, Space O(1+N)
-        ans = ""
-        spt = ""
-        flag = ""
-        lt = len(s); rt = 0
-        # only hold values in the number of embbeded layer
-        stkl = []; stkr = []
-        for i in range(len(s)):
-            flag = "no"
-            if s[i] == '[': 
-                flag = "lt"
-                stkl.append(i)
-            elif s[i] == ']':
-                stkr.append(i)
-                if len(stkl)-len(stkr)>=2: continue
-                tp = 0
-                while True:
-                    tp = stkl.pop()
-                    if len(stkl)<2: break
-                    
-                lt = tp-1
-                while True:
-                    if s[lt].isnumeric(): lt -= 1
-                    else: break
-                lt += 1
-                rt = stkr.pop()
-                if len(stkl)>0:
-                    # embedded coding string
-                    flag = "em"
-                    ss = s[lt:rt+1]
-                    print(i, ss)
-                    stp = Solution().decodeString(ss)
-                    spt += stp
-                elif len(stkl)==0:
-                    # flat coding string
-                    flag = "plain"
-                    ntp = int(s[lt:tp])
-                    ans += ntp*spt
-                    spt = ""
-            elif s[i].isnumeric():
-                flag = "num"
-            # Uncoded character
-            elif not s[i].isnumeric(): 
-                flag = "char"
-                if len(stkl)==1: 
-                    spt += s[i]
-                elif len(stkl)==0: ans += s[i]
-            print(i, s[i], flag, stkl, stkr, spt, ans)
-        return ans
+        # Time O(N), Space O(N)
+        stack = []
+        current_count = 0
+        current_string = ''        
+        for char in s:
+            if char.isdigit():
+                current_count = current_count * 10 + int(char)
+            elif char == '[':
+                stack.append((current_string, current_count))
+                current_string = ''
+                current_count = 0
+            elif char == ']':
+                prev_string, count = stack.pop()
+                current_string = prev_string + current_string * count
+            else:
+                current_string += char        
+        return current_string
+
                     
 def test_answer():
     S = Solution()
